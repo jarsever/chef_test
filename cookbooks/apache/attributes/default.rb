87681ca256291a81f2009ee501e4865b450c9f2d
Jared Everett <jared.everett@Jareds-iMac.local>
2014-01-29 11:56:15 -0700

    Last commit for the labs

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,9 +1,9 @@
-case node["platform"]
-when "ubuntu"
+case node["platform_family"]
+when "debian"  # (debian, ubuntu)
 	default["package_name"] = "apache2"
 	default["service_name"] = "apache2"
 	default["document_root"] = "/var/www"
-when "centos"
+when "rhel"    # (rhel, centos, scientific, amazon, ...)
 	default["package_name"] = "httpd"
 	default["service_name"] = "httpd"
 	default["document_root"] = "/var/www/html"
---
+++ environments/production.rb
@@ -0,0 +1,5 @@
+
+
+name "production"
+description "for production"
+cookbook "apache", "= 0.2.0"
